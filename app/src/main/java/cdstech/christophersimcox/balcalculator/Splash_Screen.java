df3798e30db6be239ba83e8d9a8e03f11031ce0f
cds327WebTech <webtechchris@nau.edu>
2017-02-07 11:41:43 -0700

    Init commit clean project

---
+++ app/src/main/java/cdstech/christophersimcox/balcalculator/Splash_Screen.java
@@ -0,0 +1,13 @@
+package cdstech.christophersimcox.balcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+    }
+}
